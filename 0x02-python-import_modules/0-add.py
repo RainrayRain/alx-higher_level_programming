8f4141570d789f958527f7f3be91e628b7ce9f96
RainrayRain <johnolere96@gmail.com>
2023-01-05 18:16:48 -0800

    add task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bash/python3
+import add_0.py as add:
+def add(a, b):
+
+a = 1
+b = 2
+    print("{}".format(add_0.add))
+if __name__ == "__main__":
---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) -1 == 0:
+        print("{} argument".format(len(sys.argv) - 1))
+         
