838b6f2378f1a1b41fd36a049cfe648e1963b94d
RainrayRain <johnolere96@gmail.com>
2023-01-24 03:31:48 -0800

    function that divides 2 integers amd print result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,4 +8,4 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("inside result: {}".format(result))
+        print("Inside result: {}".format(result))
